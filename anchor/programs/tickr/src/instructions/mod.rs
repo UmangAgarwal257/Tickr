a0ae8d832f0d92066a7ece1b96053990616e9dce
Umang Agarwal <umangagarwal257@gmail.com>
2025-05-05 17:24:47 +0530

    add withdraw funds instruction

--- anchor/programs/tickr/src/instructions/mod.rs
+++ anchor/programs/tickr/src/instructions/mod.rs
@@ -3,9 +3,11 @@ pub mod initialize;
 pub mod list;
 pub mod manager_setup;
 pub mod purchase;
+pub mod withdraw_funds;
 
 pub use delist::*;
 pub use initialize::*;
 pub use list::*;
 pub use manager_setup::*;
 pub use purchase::*;
+pub use withdraw_funds::*;
