45201af2611b90a0ab43fe6a759a1291c9789fcb
Umang Agarwal <umangagarwal257@gmail.com>
2025-05-11 17:16:54 +0530

    add create event instruction

--- anchor/programs/tickr/src/instructions/mod.rs
+++ anchor/programs/tickr/src/instructions/mod.rs
@@ -1,13 +1,15 @@
+pub mod create_event;
 pub mod delist;
 pub mod initialize;
 pub mod list;
 pub mod manager_setup;
 pub mod purchase;
-pub mod withdraw_funds;
+pub mod withdraw;
 
+pub use create_event::*;
 pub use delist::*;
 pub use initialize::*;
 pub use list::*;
 pub use manager_setup::*;
 pub use purchase::*;
-pub use withdraw_funds::*;
+pub use withdraw::*;
